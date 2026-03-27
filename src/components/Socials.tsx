b6fd9872b6a13b6b1ca76332384427733cea8d1b
jaydendeveloper <contact.jaydendev@gmail.com>
2026-03-27 13:16:44 +0100

    feat: add social links with logo

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,6 +1,31 @@
+import Image from "next/image";
 import TitleBlock from "./TitleBlock";
 
 const Socials = () => {
+	const socials = [
+		{
+			name: "E-mail",
+			link: "mailto:contact.jaydendev@gmail.com",
+			image: "https://s.magecdn.com/social/tc-mail.svg",
+		},
+		{
+			name: "GitHub",
+			link: "https://github.com/jaydendeveloper",
+			image: "https://s.magecdn.com/social/tc-github.svg",
+		},
+
+		{
+			name: "LinkedIn",
+			link: "https://www.linkedin.com/in/patrik-gyalog-63105b370/",
+			image: "https://s.magecdn.com/social/tc-linkedin.svg",
+		},
+		{
+			name: "Discord",
+			link: "https://discord.com/users/414170037478490123",
+			image: "https://s.magecdn.com/social/tc-discord.svg",
+		},
+	];
+
 	return (
 		<div className="mb-16">
 			<TitleBlock title="Socials" />
@@ -10,30 +35,31 @@ const Socials = () => {
 					Get in touch
 				</h2>
 			</div>
-			<p className="text-2xl leading-[26px] text-balance font-sans text-center ">
+			<div className="text-2xl leading-[26px] text-balance font-sans text-center ">
 				{/* github and email */}
 				<span className="opacity-80">
 					Lets connect! Feel free to reach out to me via{" "}
 				</span>
-				<a
-					href="mailto:contact.jaydendev@gmail.com"
-					className="opacity-100 text-white underline hover:opacity-80 transition-opacity duration-300"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					E-mail
-				</a>{" "}
-				<span className="opacity-80">or find me on </span>
-				<a
-					href="https://github.com/jaydendeveloper"
-					className="opacity-100 underline hover:opacity-80 transition-opacity duration-300"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					GitHub
-				</a>
-				!
-			</p>
+				<div className="flex justify-center gap-4 mt-4">
+					{socials.map((social) => (
+						<a
+							key={social.name}
+							href={social.link}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="opacity-100 text-white underline hover:opacity-80 transition-opacity duration-300"
+						>
+							<Image
+								src={social.image}
+								alt={social.name}
+								width={48}
+								height={48}
+								className="rounded-full"
+							/>
+						</a>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 };
